9ce3370585596d0330cabe990be941c47ef1b622
shoubhikraj <shoubhikrajmaiti@gmail.com>
2022-10-27 15:49:44 +0100

    minor updates

--- adegui/config.py
+++ adegui/config.py
@@ -1,12 +1,21 @@
 # Contains the config class which is instantiated once at the beginning
 # of the program
+import platform
+import subprocess
 from typing import List
 
 class _AdeGuiConfig:
     def __init__(self):
         self.adegui_workdir: str = ''  # empty means current working dir for python
 
-        self.adegui_moleditor: str = '/Applications/Avogadro.app/Contents/MacOS/Avogadro'  # path of the molecule editor
+        if platform.system() == 'Linux':
+            self.adegui_moleditor: str = subprocess.check_output(['which','avogadro']).decode('utf-8').strip()
+            # path of the molecule editor
+        elif platform.system() == 'Darwin':
+            self.adegui_moleditor: str = subprocess.check_output(['which','Avogadro']).decode('utf-8').strip()
+            # avogadro installed in Apps
+        elif platform.system() == 'Windows':
+            self.adegui_moleditor: str = subprocess.check_output(['where','avogadro']).decode('utf-8').strip()
 
         self.ade_n_cores: int = 8  # Number of cores in autodE
         self.ade_job_name: str = ''
