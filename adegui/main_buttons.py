de96fdaa21e43ba264bd1f5e205d9860ce537b19
shoubhikraj <shoubhikrajmaiti@gmail.com>
2022-11-15 17:38:54 +0000

    update

--- adegui/main_buttons.py
+++ adegui/main_buttons.py
@@ -1,8 +1,7 @@
 # defines the buttons in the main window
 from PyQt5.QtWidgets import (QHBoxLayout, QPushButton, QWidget,
-                             qApp, QStyle)
-from PyQt5.QtCore import pyqtSlot
-from adegui.generate_script import write_ade_script_from_config
+                             qApp, QStyle, QStackedWidget)
+
 
 
 class MainButtons(QWidget):
@@ -12,29 +11,38 @@ class MainButtons(QWidget):
         # create the buttons
         btn1 = QPushButton("Run")
         btn1.setEnabled(False)  # implement run function later
-        btn2 = QPushButton("Generate")  # TODO put back and next buttons
-        btn3 = QPushButton("Exit")
+        self.btn2 = QPushButton("Back")
+
+        self.btn3 = QStackedWidget()  # button 3 has to change at the last tab
+        self.next_btn = QPushButton("Next")
+        self.generate_btn = QPushButton("Generate")
+        self.btn3.addWidget(self.next_btn)
+        self.btn3.addWidget(self.generate_btn)
+
+        btn4 = QPushButton("Exit")
 
         # add icons to buttons
         btn1.setIcon(qApp.style().standardIcon(QStyle.SP_MediaPlay))
-        btn2.setIcon(qApp.style().standardIcon(QStyle.SP_DialogSaveButton))
-        btn3.setIcon(qApp.style().standardIcon(QStyle.SP_BrowserStop))
+        self.btn2.setIcon(qApp.style().standardIcon(QStyle.SP_ArrowLeft))
+        self.next_btn.setIcon(qApp.style().standardIcon(QStyle.SP_ArrowRight))
+        self.generate_btn.setIcon(qApp.style().standardIcon(QStyle.SP_DialogSaveButton))
+        btn4.setIcon(qApp.style().standardIcon(QStyle.SP_BrowserStop))
 
-        # connect buttons to trigger functions
-        btn2.clicked.connect(self.generate_script)
-        btn3.clicked.connect(qApp.quit)
+        # connect close to trigger function
+        btn4.clicked.connect(qApp.quit)
+        # other functions will be connected in the main window
+        # so that the tabs can be changed
 
         # the buttons have to be stacked horizontally
         btn_area_layout = QHBoxLayout()
         btn_area_layout.addWidget(btn1)  # first button
-        btn_area_layout.addWidget(btn2)  # second button
+        btn_area_layout.addWidget(self.btn2)  # back button
+        btn_area_layout.addWidget(self.btn3)  # forward or generate button
         btn_area_layout.addStretch()
-        btn_area_layout.addWidget(btn3)  # third button
+        btn_area_layout.addWidget(btn4)  # third button
 
         self.setLayout(btn_area_layout)
 
-    @pyqtSlot()
-    def generate_script(self):
-        write_ade_script_from_config(self)
+
 
 
--- adegui/main_win.py
+++ adegui/main_win.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtCore import pyqtSlot
 from adegui.work_area.work_area import WorkAreaTabs
 from adegui.main_buttons import MainButtons
+from adegui.generate_script import write_ade_script_from_config
 
 
 class MainWindow(QMainWindow):
@@ -12,11 +14,36 @@ class MainWindow(QMainWindow):
         # The layout for the main window => buttons at bottom,
         # tabs and work area at top
         main_layout = QVBoxLayout()  # create a vertical stacked layout
-        main_layout.addWidget(WorkAreaTabs())  # first part of vertical layout <= main work area
-        main_layout.addWidget(MainButtons())  # second part of vertical layout <= buttons
+        self.work_area_tabs = WorkAreaTabs()
+        self.main_btns = MainButtons()
+        main_layout.addWidget(self.work_area_tabs)  # first part of vertical layout <= main work area
+        main_layout.addWidget(self.main_btns)  # second part of vertical layout <= buttons
+        self.main_btns.btn2.clicked.connect(self.prev_tab)
+        self.main_btns.next_btn.clicked.connect(self.next_tab)
+        self.main_btns.generate_btn.clicked.connect(self.generate_script)
 
         # QMainWindow already has a fixed layout, so create a widget then
         # apply layout on it, and set it as the central widget
         main_widget = QWidget()
         main_widget.setLayout(main_layout)
         self.setCentralWidget(main_widget)
+
+    @pyqtSlot()
+    def next_tab(self):
+        current_tab_idx = self.work_area_tabs.currentIndex()
+        if current_tab_idx < (self.work_area_tabs.count()-1):
+            self.work_area_tabs.setCurrentIndex(current_tab_idx+1)  # go to next tab
+        if (self.work_area_tabs.count()-1) == (current_tab_idx+1):  # if at last tab
+            self.main_btns.btn3.setCurrentIndex(1)  # show generate button
+
+    @pyqtSlot()
+    def prev_tab(self):
+        current_tab_idx = self.work_area_tabs.currentIndex()
+        if current_tab_idx > 0:
+            self.work_area_tabs.setCurrentIndex(current_tab_idx-1)  # go to previous tab
+        if current_tab_idx <= (self.work_area_tabs.count()-1):  # if not at last tab
+            self.main_btns.btn3.setCurrentIndex(0)
+
+    @pyqtSlot()
+    def generate_script(self):
+        write_ade_script_from_config(self)
