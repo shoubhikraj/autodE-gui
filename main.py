af891c55941c4b1ae062d0cd3e5ba5ed3d9d33c7
shoubhikraj <shoubhikrajmaiti@gmail.com>
2022-11-20 22:26:48 +0000

    ui changes

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from adegui.main_win import MainWindow
+from adegui.stylesheet import adegui_style_sheet
 
 if __name__ == '__main__':
     app = QApplication([])
+    app.setStyle("Fusion")
+    #app.setStyleSheet(adegui_style_sheet)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
